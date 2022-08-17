39b192e7e4bf6d2266111a67e0c4a27893eadf27
Büşra Işık <83750442+BusraIsik@users.noreply.github.com>
2022-08-17 21:31:09 +0300

    added update and delete buttons

--- AgeCalculator/AgeCalculator/Controllers/UserController.cs
+++ AgeCalculator/AgeCalculator/Controllers/UserController.cs
@@ -16,5 +16,57 @@ namespace AgeCalculator.Controllers
             var liste = db.Users.ToList();
             return View(liste);
         }
+
+        public JsonResult getUser(int userId)
+        {
+            Users user = db.Users.Find(userId);
+            var userObjects = new
+            {
+                name = user.Name,
+                surname = user.Surname,
+                birthdate = user.Birthdate.ToString("yyyy-MM-dd"),
+                gender = user.Gender,
+                city = user.City
+            };
+
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult addUser(string name, string surname, DateTime birthdate, string gender, string city)
+        {
+            var age = DateTime.Today.Year - birthdate.Year;
+            Users user = new Users();
+            user.Name = name;
+            user.Surname = surname;
+            user.Birthdate = birthdate;
+            user.Gender = gender;
+            user.City = city;
+            user.Age = age;
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Json("");
+        }
+
+        public JsonResult deleteUser(int userId)
+        {
+            Users user = db.Users.Find(userId);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return Json("");
+        }
+
+        public JsonResult updateUser(int userId, string name, string surname, DateTime birthdate, string gender, string city)
+        {
+            var age = DateTime.Today.Year - birthdate.Year;
+            Users user = db.Users.Find(userId);
+            user.Name = name;
+            user.Surname = surname;
+            user.Birthdate = birthdate;
+            user.Gender = gender;
+            user.City = city;
+            user.Age = age;
+            db.Entry(user);
+            db.SaveChanges();
+            return Json("");
+        }
     }
 }
